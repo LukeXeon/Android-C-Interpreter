84c0746b55c205c8c0aabecdbd15c0298955a9eb
Luke Luo <694014820@qq.com>
2018-11-07 23:25:29 +0800

    upload

--- apicoc/apicoc.cpp
+++ apicoc/apicoc.cpp
@@ -4,29 +4,51 @@
 #define PICOC_STACK_SIZE (128*1024)              /* space for the the stack */
 
 
-static struct JNICaches {
+struct CatchInfo {
+	struct sigaction oldAction;
+	sigjmp_buf jumpBuffer;
+};
+
+static struct {
+	JavaVM*javaVM;
 	jfieldID descriptorField;
-	jclass runtimeExClass;
+	jclass accessExClass;
+	mutex catchInfoMutex;
+	map<pid_t, CatchInfo> catchInfo;
 } caches;
 
 static void SetFd(JNIEnv *env, jobject fileDescriptor, int value) {
 	env->SetIntField(fileDescriptor, caches.descriptorField, value);
 }
 
-static Picoc*Cast(jlong ptr) {
-	return (Picoc *) ptr;
+static Picoc*ToPtr(jlong ptr) {
+	return (Picoc *)ptr;
 }
 
 static jlong ToLong(Picoc*ptr) {
-	return (jlong) ptr;
+	return (jlong)ptr;
 }
 
+EXTERN_C
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM* vm, void *reserved)
+{
+	LOGI("%s", "apicoc lib load");
+	caches.javaVM = vm;
+	JNIEnv*env = nullptr;
+	vm->GetEnv((void**)(&env), JNI_VERSION_1_6);
+	caches.descriptorField =
+		env->GetFieldID(env->FindClass("java/io/FileDescriptor"), "descriptor", "I");
+	caches.accessExClass =
+		(jclass)env->NewGlobalRef(env->FindClass("java/lang/IllegalAccessException"));
+	return JNI_VERSION_1_6;
+}
 
 EXTERN_C
 JNIEXPORT jint JNICALL
 Java_edu_guet_apicoc_Interpreter_createSub0(JNIEnv *env, jclass type, jobjectArray srcNames,
-											jobjectArray srcOrFile, jobjectArray args,
-											jobject stdinFd, jobject stdoutFd, jobject stderrFd) {
+	jobjectArray srcOrFile, jobjectArray args,
+	jobject stdinFd, jobject stdoutFd, jobject stderrFd) {
 	int child_pid;
 	int stdin_pipe[2];
 	int stdout_pipe[2];
@@ -45,20 +67,20 @@ Java_edu_guet_apicoc_Interpreter_createSub0(JNIEnv *env, jclass type, jobjectArr
 	if (srcNames != nullptr) {
 		c_src_names = new char *[src_or_file_count];
 		for (int i = 0; i < src_or_file_count; ++i) {
-			jstring jstr = (jstring) env->GetObjectArrayElement(srcNames, i);
+			jstring jstr = (jstring)env->GetObjectArrayElement(srcNames, i);
 			const char *jstr_chars = env->GetStringUTFChars(jstr, 0);
 			c_src_names[i] = strcpy(new char[strlen(jstr_chars) + 1], jstr_chars);
 			env->ReleaseStringUTFChars(jstr, jstr_chars);
 		}
 	}
 	for (int i = 0; i < src_or_file_count; ++i) {
-		jstring jstr = (jstring) env->GetObjectArrayElement(srcOrFile, i);
+		jstring jstr = (jstring)env->GetObjectArrayElement(srcOrFile, i);
 		const char *jstr_chars = env->GetStringUTFChars(jstr, 0);
 		src_or_file_path[i] = strcpy(new char[strlen(jstr_chars) + 1], jstr_chars);
 		env->ReleaseStringUTFChars(jstr, jstr_chars);
 	}
 	for (int i = 0; i < argc; ++i) {
-		jstring jstr = (jstring) env->GetObjectArrayElement(args, i);
+		jstring jstr = (jstring)env->GetObjectArrayElement(args, i);
 		const char *jstr_chars = env->GetStringUTFChars(jstr, 0);
 		c_args[i] = strcpy(new char[strlen(jstr_chars) + 1], jstr_chars);
 		env->ReleaseStringUTFChars(jstr, jstr_chars);
@@ -86,19 +108,19 @@ Java_edu_guet_apicoc_Interpreter_createSub0(JNIEnv *env, jclass type, jobjectArr
 		PicocInitialise(picoc, getenv("STACKSIZE") ? atoi(getenv("STACKSIZE")) : PICOC_STACK_SIZE);
 		PicocIncludeAllSystemHeaders(picoc);
 		picoc->CStdIn = fdopen(stdin_pipe[0], "r");
-		dup2(stdin_pipe[0],STDIN_FILENO);
+		dup2(stdin_pipe[0], STDIN_FILENO);
 		picoc->CStdOut = fdopen(stdout_pipe[1], "w");
 		setvbuf(picoc->CStdOut, NULL, _IONBF, 0);
-		dup2(stdout_pipe[1],STDOUT_FILENO);
+		dup2(stdout_pipe[1], STDOUT_FILENO);
 		picoc->CStdErr = fdopen(stderr_pipe[1], "w");
 		setvbuf(picoc->CStdErr, NULL, _IONBF, 0);
-		dup2(stderr_pipe[1],STDERR_FILENO);//重定向到管道
+		dup2(stderr_pipe[1], STDERR_FILENO);//重定向到管道
 		if (c_src_names != nullptr) {
 			for (int i = 0; i < src_or_file_count; ++i) {
 				PicocParse(picoc, c_src_names[i],
-						   src_or_file_path[i],
-						   (int)strlen(src_or_file_path[i]),
-						   true, false, true, true);
+					src_or_file_path[i],
+					(int)strlen(src_or_file_path[i]),
+					true, false, true, true);
 			}
 		}
 		else {
@@ -126,11 +148,10 @@ Java_edu_guet_apicoc_Interpreter_killSub0(JNIEnv *env, jclass type, jint pid) {
 	kill(pid, SIGKILL);
 }
 
-
 EXTERN_C
 JNIEXPORT jlong JNICALL
 Java_edu_guet_apicoc_Interpreter_init0(JNIEnv *env, jclass type, jobject stdinFd, jobject stdoutFd,
-									   jobject stderrFd) {
+	jobject stderrFd) {
 	LOGI("picoc init");
 	Picoc *picoc = new Picoc();
 	PicocInitialise(picoc, getenv("STACKSIZE") ? atoi(getenv("STACKSIZE")) : PICOC_STACK_SIZE);
@@ -151,17 +172,6 @@ Java_edu_guet_apicoc_Interpreter_init0(JNIEnv *env, jclass type, jobject stdinFd
 	SetFd(env, stderrFd, stderr_pipe[0]);
 	return ToLong(picoc);
 }
-EXTERN_C
-JNIEXPORT void JNICALL
-Java_edu_guet_apicoc_Interpreter_staticInit0(JNIEnv *env, jclass type) {
-	jclass fileDescriptorClass =
-			env->FindClass("java/io/FileDescriptor");
-	caches.descriptorField =
-			env->GetFieldID(fileDescriptorClass, "descriptor", "I");
-
-	caches.runtimeExClass =
-			(jclass) env->NewGlobalRef(env->FindClass("java/lang/RuntimeException"));
-}
 
 EXTERN_C
 JNIEXPORT void JNICALL
@@ -169,10 +179,10 @@ Java_edu_guet_apicoc_Interpreter_addSource0(JNIEnv *env, jclass type, jlong ptr,
 	LOGI("add source");
 	const char *ramdonName = env->GetStringUTFChars(ramdonName_, 0);
 	const char *source = env->GetStringUTFChars(source_, 0);
-	PicocParse(Cast(ptr), ramdonName,
-			   env->GetStringUTFChars(source_, 0),
-			   env->GetStringLength(source_),
-			   true, false, true, true);
+	PicocParse(ToPtr(ptr), ramdonName,
+		env->GetStringUTFChars(source_, 0),
+		env->GetStringLength(source_),
+		true, false, true, true);
 	env->ReleaseStringUTFChars(ramdonName_, ramdonName);
 	env->ReleaseStringUTFChars(source_, source);
 }
@@ -184,7 +194,7 @@ Java_edu_guet_apicoc_Interpreter_cleanup0(JNIEnv *env, jclass type, jlong ptr) {
 		return;
 	}
 	LOGI("picoc cleanup");
-	Picoc *picoc = Cast(ptr);
+	Picoc *picoc = ToPtr(ptr);
 	PicocCleanup(picoc);
 }
 
@@ -195,7 +205,7 @@ Java_edu_guet_apicoc_Interpreter_close0(JNIEnv *env, jclass type, jlong ptr) {
 		return;
 	}
 	LOGI("picoc close");
-	Picoc *pc = Cast(ptr);
+	Picoc *pc = ToPtr(ptr);
 	close(fileno(pc->CStdIn));
 	close(fileno(pc->CStdOut));
 	close(fileno(pc->CStdErr));
@@ -207,16 +217,40 @@ EXTERN_C
 JNIEXPORT jint JNICALL
 Java_edu_guet_apicoc_Interpreter_callMain0(JNIEnv *env, jclass type, jlong ptr, jobjectArray args) {
 	LOGI("picoc call main");
-	Picoc *picoc = Cast(ptr);
+	struct sigaction act;
+	pid_t pid = getpid();
+	Picoc *picoc = ToPtr(ptr);
+	caches.catchInfoMutex.lock();
+	CatchInfo &catchInfo = caches.catchInfo.emplace(pid, CatchInfo()).first->second;
+	caches.catchInfoMutex.unlock();
+	act.sa_handler = [](int sig) -> void {
+		CatchInfo &info = caches.catchInfo[getpid()];
+		sigaction(SIGSEGV, &(info.oldAction), NULL);
+		siglongjmp(info.jumpBuffer, 1);
+	};
+	sigaction(SIGSEGV, &act, &(catchInfo.oldAction));
 	jsize size = env->GetArrayLength(args);
 	char **cargs = new char *[size];
 	for (jsize i = 0; i < size; i++) {
-		cargs[i] = (char *) env->GetStringUTFChars((jstring) env->GetObjectArrayElement(args, i),
-												   JNI_FALSE);
+		cargs[i] = (char *)env->GetStringUTFChars((jstring)env->GetObjectArrayElement(args, i),
+			JNI_FALSE);
 	}
-	PicocCallMain(Cast(ptr), size, cargs);
+	if (sigsetjmp(catchInfo.jumpBuffer, 1) == 0) {
+		PicocCallMain(ToPtr(ptr), size, cargs);
+	}
+	else {
+		for (jsize i = 0; i < size; i++) {
+			env->ReleaseStringUTFChars((jstring)env->GetObjectArrayElement(args, i), cargs[i]);
+			cargs[i] = nullptr;
+		}
+		delete cargs;
+		caches.catchInfoMutex.lock();
+		caches.catchInfo.erase(pid);
+		caches.catchInfoMutex.unlock();
+		env->ThrowNew(caches.accessExClass, "signal : SIGSEGV");
+	}
 	for (jsize i = 0; i < size; i++) {
-		env->ReleaseStringUTFChars((jstring) env->GetObjectArrayElement(args, i), cargs[i]);
+		env->ReleaseStringUTFChars((jstring)env->GetObjectArrayElement(args, i), cargs[i]);
 		cargs[i] = nullptr;
 	}
 	delete cargs;
@@ -228,6 +262,6 @@ JNIEXPORT void JNICALL
 Java_edu_guet_apicoc_Interpreter_addFile0(JNIEnv *env, jclass type, jlong ptr, jstring path_) {
 	LOGI("add File");
 	const char *path = env->GetStringUTFChars(path_, 0);
-	PicocPlatformScanFile(Cast(ptr), path);
+	PicocPlatformScanFile(ToPtr(ptr), path);
 	env->ReleaseStringUTFChars(path_, path);
 }
--- apicoc/apicoc.h
+++ apicoc/apicoc.h
@@ -12,8 +12,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <android/log.h>
+#include <map>
+#include <mutex>
 BINGE_EXTERN_C
 #include "picoc.h"
 END_EXTERN_C
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "apicoc", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "apicoc", __VA_ARGS__))
+using namespace std;
